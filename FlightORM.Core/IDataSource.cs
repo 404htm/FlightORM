28526f1f30e5f72f542ba229e55f3ac5893b2ed7
404htm <github@404htm.com>
2016-02-01 17:02:15 -0500

    Project Structure Changes, Misc Fixes.

--- FlightORM.Core/IDataSource.cs
+++ FlightORM.Core/IDataSource.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace FlightORM.Core
 {
-    public class Class1
+    interface IDataSource
     {
+        Guid Id { get; set; }
+        string Name { get; set; }
     }
 }
