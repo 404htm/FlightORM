e34637d9e3810362e5f9e7751b2e0cae3ca9f30e
404htm <github@404htm.com>
2016-02-01 22:56:20 -0500

    Added code to read tables/views from DB metadata. Added tests for tables.

---
+++ FlightORM.SQL.Tests/SQLSchemaReaderTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FlightORM.SQL.Tests
+{
+    [TestClass]
+    public class SQLSchemaReaderTests
+    {
+        string _cstr;
+        string _cstr_db;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _cstr = Properties.Settings.Default.cnn_server;
+            _cstr_db = Properties.Settings.Default.cnn_db;
+        }
+
+        [TestMethod]
+        public void GetAllTablesWithSchema()
+        {
+            List<string> tables;
+            using (var reader = new SQLSchemaReader(_cstr_db))
+            {
+                tables = reader.GetTables(null, true).ToList();
+            }
+
+            Assert.IsTrue(tables.Contains("dbo.Customer"), "dbo.Customer not found");
+            Assert.IsTrue(tables.Contains("dbo.Order"), "dbo.Order not found");
+            Assert.IsTrue(tables.Contains("inventory.Item"), "inventory.Item not found");
+        }
+
+        [TestMethod]
+        public void GetAllTablesNoSchema()
+        {
+            List<string> tables;
+            using (var reader = new SQLSchemaReader(_cstr_db))
+            {
+                tables = reader.GetTables(null, false).ToList();
+            }
+
+            Assert.IsTrue(tables.Contains("Customer"), "dbo.Customer not found");
+            Assert.IsTrue(tables.Contains("Order"), "dbo.Order not found");
+            Assert.IsTrue(tables.Contains("Item"), "inventory.Item not found");
+        }
+
+        [TestMethod]
+        public void GetdboTablesWithSchema()
+        {
+            List<string> tables;
+            using (var reader = new SQLSchemaReader(_cstr_db))
+            {
+                tables = reader.GetTables("dbo", true).ToList();
+            }
+
+            Assert.IsTrue(tables.Contains("dbo.Customer"), "dbo.Customer not found");
+            Assert.IsTrue(tables.Contains("dbo.Order"), "dbo.Order not found");
+            Assert.IsFalse(tables.Contains("inventory.Item"), "inventory.Item not found");
+        }
+
+        [TestMethod]
+        public void GetdboTablesNoSchema()
+        {
+            List<string> tables;
+            using (var reader = new SQLSchemaReader(_cstr_db))
+            {
+                tables = reader.GetTables("dbo", false).ToList();
+            }
+
+            Assert.IsTrue(tables.Contains("Customer"), "dbo.Customer not found");
+            Assert.IsTrue(tables.Contains("Order"), "dbo.Order not found");
+            Assert.IsFalse(tables.Contains("Item"), "inventory.Item not found");
+        }
+    }
+}
---
+++ FlightORM.SQL/SQLSchemaReader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightORM.SQL
+{
+    public class SQLSchemaReader : IDisposable
+    {
+        SqlConnection _cnn;
+
+        public SQLSchemaReader(string connectionString)
+        {
+            _cnn = new SqlConnection(connectionString);
+            _cnn.Open();
+        }
+
+
+        public IEnumerable<string> GetTables(string schema = null, bool includeSchema = true)
+        {
+            return RunInfoSchemaQuery(schema, includeSchema, "BASE TABLE");
+        }
+
+        public IEnumerable<string> GetViews(string schema = null, bool includeSchema = true)
+        {
+            return RunInfoSchemaQuery(schema, includeSchema, "VIEW");
+        }
+
+        private IEnumerable<string> RunInfoSchemaQuery(string schema, bool includeSchema, string type)
+        {
+            string query = "SELECT * FROM information_schema.tables WHERE TABLE_TYPE = @type";
+            if (schema != null) query += " AND TABLE_SCHEMA = @schema";
+
+            var command = new SqlCommand(query, _cnn);
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@type", type);
+            if (schema != null) command.Parameters.AddWithValue("@schema", schema);
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string result = (includeSchema) ?
+                    $"{reader["TABLE_SCHEMA"]}.{reader["TABLE_NAME"]}" :
+                    $"{reader["TABLE_NAME"]}";
+                yield return result;
+            }
+
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _cnn.Close();
+                }
+                disposedValue = true;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
